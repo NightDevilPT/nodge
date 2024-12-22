2de6f6e3d58f545841550c955ea539827b2ddaec
NightDevilPT <pawankumartadagsingh@gmail.com>
2024-12-22 09:13:10 +0530

    frontend:nodge-added

--- src/app/fonts/index.ts
+++
@@ -1,8 +0,0 @@
-
-import localFont from "next/font/local";
-
-export const rubikFont = localFont({
-	src: "./rubik/Rubik-VariableFont_wght.ttf",
-	variable: "--font-rubik",
-	weight: "100 900",
-});
--- src/components/react-flow/nodes/index.ts
+++
@@ -1,7 +0,0 @@
-import DefaultNode from "./DefaultNode";
-import InputNode from "./InputNode";
-import OutputNode from "./OutputNode";
-
-export { InputNode, OutputNode, DefaultNode };
-
-export type NodeType = 'input' | 'default' | 'output';
